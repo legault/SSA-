11690426cc1d4d49cc12ba2950bd8dade45ec201
Geoffrey Legault <legault@users.noreply.github.com>
2018-04-27 16:19:32 -0400

    Add files via upload
    changed label for varying/irregular

--- results-figure1.R
+++ results-figure1.R
@@ -4,19 +4,27 @@ times <- seq(0, 10, 1)
 times2 <- seq(0, 10, .01)
 
 ## Source increasing1 environment
-(source("Environments/increasing1.R"))
+source("Environments/increasing1.R")
+## View structure
+increasing1
 ## Source increasing2 environment
-(source("Environments/increasing2.R"))
+source("Environments/increasing2.R")
+## View structure
+increasing2
 ## Source fluctuating1 environment
-(source("Environments/fluctuating1.R"))
+source("Environments/fluctuating1.R")
+## View structure
+fluctuating1
 ## Source fluctuating2 environment
-(source("Environments/fluctuating2.R"))
-# Random1
-## Create random environmental values
+source("Environments/fluctuating2.R")
+## View structure
+fluctuating2
+# Irregular1
+## Create irregular environmental values
 set.seed(20170731)
 randenv1 <- rnorm(length(times), mean = 1.5, sd = .25)
 randenv1 <- splinefun(times, randenv1)
-# Random2
+# Irregular2
 set.seed(20170731)
 randenv2 <- rnorm(length(times), mean = 1.5, sd = .5)
 randenv2 <- splinefun(times, randenv2)
@@ -47,7 +55,7 @@ points(fluctuating1(times2) ~ times2, type = "l", col = "black", lwd = 3, lty = 
 points(fluctuating2(times2) ~ times2, type = "l", col = "black", lwd = 2, lty = "dotted")
 legend("topright", c("fluctuating 1", "fluctuating 2"), bty = "n", inset = .02, adj = c(0, 0), lty = c("dashed", "dotted"), lwd = c(3, 2), cex = 1.3)
 mtext(side = 3, c("(b)"), adj = 0)
-## Random
+## Irregular
 plot(NA, xlab = expression(paste("Time (", italic(t), ")")), ylab = "",
      xlim = c(0, 10), ylim = c(0, 4),
      xaxt = "n", yaxt = "n", bty = "n",
@@ -56,6 +64,6 @@ axis(1, at = seq(0, 10, 2), tcl = -.5, cex = 1.2)
 axis(2, at = seq(0, 4, 1), tcl = -.5, las = 1)
 points(randenv1(times2) ~ times2, type = "l", col = "black", lwd = 3, lty = "dashed")
 points(randenv2(times2) ~ times2, type = "l", col = "black", lwd = 2, lty = "dotted")
-legend("topright", c("random 1", "random 2"), bty = "n", inset = .02, adj = c(0, 0), lty = c("dashed", "dotted"), lwd = c(3, 2), cex = 1.3)
+legend("topright", c("irregular 1", "irregular 2"), bty = "n", inset = .02, adj = c(0, 0), lty = c("dashed", "dotted"), lwd = c(3, 2), cex = 1.3)
 mtext(side = 3, c("(c)"), adj = 0)
 dev.off()
--- results-figure2.R
+++ results-figure2.R
@@ -1,59 +1,78 @@
-## Source nhppCDF function (CDF for non-stationary process)
-(source("nhppCDF.R"))
+## Source nhpptimes function (CDF for non-stationary processes)
+source("nhpptimes.R")
+## View structure
+nhpptimes
 
 ## Source constant environment
-(source("Environments/constant.R"))
+source("Environments/constant.R")
+## View structure
+constant
 ## Source increasing1 environment
-(source("Environments/increasing1.R"))
+source("Environments/increasing1.R")
+## View structure
+increasing1
 ## Source increasing2 environment
-(source("Environments/increasing2.R"))
+source("Environments/increasing2.R")
+## View structure
+increasing2
 ## Source fluctuating1 environment
-(source("Environments/fluctuating1.R"))
+source("Environments/fluctuating1.R")
+## View structure
+fluctuating1
 ## Source fluctuating2 environment
-(source("Environments/fluctuating2.R"))
+source("Environments/fluctuating2.R")
+## View structure
+fluctuating2
 
 # Exponential growth
 ## Source environment-dependent function for exponential growth
-(source("Demographic functions/exponentialfun.R"))
+source("birthdeath/Demographic functions/exponentialfun.R")
+## View structure
+exponentialfun
 ## Source intensity function
-(source("Intensity functions/exponentialINT.R"))
+source("birthdeath/Intensity functions/exponentialINT.R")
+## View structure
+exponentialINT
+
 # Logistic growth
 ## Source environment-dependent function for logistic growth
-(source("Demographic functions/logisticfun.R"))
+source("Demographic functions/logisticfun.R")
+## View structure
+logisticfun
 ## Source intensity function
-(source("Intensity functions/logisticINT.R"))
+source("Intensity functions/logisticINT.R")
+## View structure
+logisticINT
 
-# Global values
-init <- 100 # initial population size
-times <- seq(0, 10, .1) # time points
 
-# Save plot
-## pdf(file="Figures/fig2.pdf", width = 7, height = 7)
-# Set layout
+pdf(file="Figures/fig2.pdf", width = 7, height = 7)
 par(mfrow = c(2, 3), mar = c(5, 6, 2, 1), lend = 2, cex.axis = 1.3, cex.lab = 1.5)
-
-##### EXPONENTIAL GROWTH
-########## Increasing
-# Parameters, exponential growth (increasing1)
+# Exponential initial values
+init <- 100
+# Exponential growth (increasing1)
 param1 <- list(b = .003,
                d = .0027,
                exponentialfun,
                env = increasing1)
 param1 <- unlist(param1) # collapse to one-dimensional list
-# Parameters, exponential growth (increasing2)
+# Exponential growth (increasing2)
 param2 <- list(b = .003,
                d = .0027,
                exponentialfun,
                env = increasing2)
 param2 <- unlist(param2) # collapse to one-dimensional list
+## Create state change array
+pproc <- matrix(c(1))
+## Times
+times <- seq(0, 10, .1)
 ## Make CDFs
 ssa.cdf <- c()
 ssaplus1.cdf <- c()
 ssaplus2.cdf <- c()
 for(i in 1:length(times)){
     ssa.cdf[i] <- 1 - exp(-sum(exponentialINT(0, init, param1)) * times[i])
-    ssaplus1.cdf[i] <- nhppCDF(times[i], init, param1, exponentialINT)
-    ssaplus2.cdf[i] <- nhppCDF(times[i], init, param2, exponentialINT)
+    ssaplus1.cdf[i] <- nhpptimes(times[i], init, param1, exponentialINT)
+    ssaplus2.cdf[i] <- nhpptimes(times[i], init, param2, exponentialINT)
 }
 # Plots
 plot(NA, xlab = "", ylab = "",
@@ -68,28 +87,30 @@ points(ssaplus1.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dashed")
 points(ssaplus2.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dotted")
 mtext(side = 3, c("(a)"), adj = 0)
 legend("bottomright",  c("SSAn", "SSA+ (inc. 1)", "SSA+ (inc. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
-
-########## Fluctuating
-# Parameters, exponential growth (fluctuating1)
+# Exponential growth (fluctuating1)
 param1 <- list(b = .003,
                d = .0027,
                exponentialfun,
                env = fluctuating1)
 param1 <- unlist(param1) # collapse to one-dimensional list
-# Parameters, exponential growth (fluctuating2)
+# Exponential growth (fluctuating2)
 param2 <- list(b = .003,
                d = .0027,
                exponentialfun,
                env = fluctuating2)
 param2 <- unlist(param2) # collapse to one-dimensional list
+## Create state change array
+pproc <- matrix(c(1))
+## Times
+times <- seq(0, 10, .1)
 ## Make CDFs
 ssa.cdf <- c()
 ssaplus1.cdf <- c()
 ssaplus2.cdf <- c()
 for(i in 1:length(times)){
     ssa.cdf[i] <- 1 - exp(-sum(exponentialINT(0, init, param1)) * times[i])
-    ssaplus1.cdf[i] <- nhppCDF(times[i], init, param1, exponentialINT)
-    ssaplus2.cdf[i] <- nhppCDF(times[i], init, param2, exponentialINT)
+    ssaplus1.cdf[i] <- nhpptimes(times[i], init, param1, exponentialINT)
+    ssaplus2.cdf[i] <- nhpptimes(times[i], init, param2, exponentialINT)
 }
 ## Plots
 plot(NA, xlab = "", ylab = "",
@@ -103,38 +124,45 @@ points(ssaplus1.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dashed")
 points(ssaplus2.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dotted")
 mtext(side = 3, c("(b)"), adj = 0)
 legend("bottomright",  c("SSAn", "SSA+ (fluc. 1)", "SSA+ (fluc. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
-
-########## Random
-## Create random1 environmental values
+# Exponential growth (irregular 1)
+## Create irregular environmental values
 set.seed(20170731)
 randenv1 <- rnorm(length(times), mean = 1.5, sd = .25)
+## View values
+randenv1
+# Exponential growth (irregular 2)
 ## Create function that interpolates these values
 randenvfun1 <- splinefun(times, randenv1)
-## Create random2 environmental values
 set.seed(20170731)
 randenv2 <- rnorm(length(times), mean = 1.5, sd = .5)
+## View values
+randenv2
 ## Create function that interpolates these values
 randenvfun2 <- splinefun(times, randenv2)
-# Parameters, exponential growth (random1)
+# Exponential growth (increasing1)
 param1 <- list(b = .003,
                d = .0027,
                exponentialfun,
                env = randenvfun1)
 param1 <- unlist(param1) # collapse to one-dimensional list
-# Parameters, exponential growth (random2)
+# Exponential growth (increasing2)
 param2 <- list(b = .003,
                d = .0027,
                exponentialfun,
                env = randenvfun2)
 param2 <- unlist(param2) # collapse to one-dimensional list
+## Create state change array
+pproc <- matrix(c(1))
+## Times
+times <- seq(0, 10, .1)
 ## Make CDFs
 ssa.cdf <- c()
 ssaplus1.cdf <- c()
 ssaplus2.cdf <- c()
 for(i in 1:length(times)){
     ssa.cdf[i] <- 1 - exp(-sum(exponentialINT(0, init, param1)) * times[i])
-    ssaplus1.cdf[i] <- nhppCDF(times[i], init, param1, exponentialINT)
-    ssaplus2.cdf[i] <- nhppCDF(times[i], init, param2, exponentialINT)
+    ssaplus1.cdf[i] <- nhpptimes(times[i], init, param1, exponentialINT)
+    ssaplus2.cdf[i] <- nhpptimes(times[i], init, param2, exponentialINT)
 }
 # Plots
 plot(NA, xlab = "", ylab = "",
@@ -147,34 +175,66 @@ points(ssa.cdf ~ times, type = "l", col = "black", lwd = 2)
 points(ssaplus1.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dashed")
 points(ssaplus2.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dotted")
 mtext(side = 3, c("(c)"), adj = 0)
-legend("bottomright",  c("SSAn", "SSA+ (rand. 1)", "SSA+ (rand. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
-
-##### LOGISTIC GROWTH
-########## Increasing
-# Parameters, logistic growth (increasing1)
+legend("bottomright",  c("SSAn", "SSA+ (irreg. 1)", "SSA+ (irreg. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
+## # Logistic growth (constant)
+## param <- list(b1 = .003,
+##               d1 = .0027,
+##               d2 = .000003,
+##               logisticfun,
+##               env = constant)
+## param <- unlist(param) # collapse to one-dimensional list
+## ## Create state change array
+## pproc <- matrix(c(1,
+##                   -1),
+##                 nrow = 2)
+## ## Times
+## times <- seq(0, 30, .1)
+## ## Make CDFs
+## ssa.cdf <- c()
+## ssaplus.cdf <- c()
+## for(i in 1:length(times)){
+##     ssa.cdf[i] <- 1 - exp(-sum(logisticINT(0, 25, param)) * times[i])
+##     ssaplus.cdf[i] <- nhpptimes(times[i], 25, param, logisticINT)
+## }
+## plot(NA, xlab = "Time", ylab = "Probability event has occurred",
+##      xlim = c(0, 30), ylim = c(0, 1),
+##      xaxt = "n", yaxt = "n", bty = "n",
+##      main = "")
+## axis(1, at = seq(0, 30, 5), tcl = -.25)
+## axis(2, at = seq(0, 1, .2), tcl = -.25, las = 1)
+## points(ssa.cdf ~ times, type = "l", col = "black", lwd = 2)
+## points(ssaplus.cdf ~ times, type = "l", col = "black", lwd = 4, lty = "dashed")
+## legend("bottomright",  c("SSA (constant)", "SSA+ (constant)"), bty = "n", inset = .05, adj = c(0, 0), lty = c("solid", "dashed"), lwd = 2)
+# Initial values
+init <- 100
+# Logistic growth (increasing)
 param1 <- list(b1 = .003,
               d1 = .0027,
               d2 = .000003,
               logisticfun,
               env = increasing1)
 param1 <- unlist(param1) # collapse to one-dimensional list
-# Parameters, logistic growth (increasing2)
 param2 <- list(b1 = .003,
               d1 = .0027,
               d2 = .000003,
               logisticfun,
               env = increasing2)
 param2 <- unlist(param2) # collapse to one-dimensional list
+## Create state change array
+pproc <- matrix(c(1,
+                  -1),
+                nrow = 2)
+## Times
+times <- seq(0, 5, .1)
 ## Make CDFs
 ssa.cdf <- c()
 ssaplus1.cdf <- c()
 ssaplus2.cdf <- c()
 for(i in 1:length(times)){
     ssa.cdf[i] <- 1 - exp(-sum(logisticINT(0, init, param1)) * times[i])
-    ssaplus1.cdf[i] <- nhppCDF(times[i], init, param1, logisticINT)
-    ssaplus2.cdf[i] <- nhppCDF(times[i], init, param2, logisticINT)
+    ssaplus1.cdf[i] <- nhpptimes(times[i], init, param1, logisticINT)
+    ssaplus2.cdf[i] <- nhpptimes(times[i], init, param2, logisticINT)
 }
-# Plots
 plot(NA, xlab = "Time", ylab = "",
      xlim = c(0, 5), ylim = c(0, 1),
      xaxt = "n", yaxt = "n", bty = "n",
@@ -187,32 +247,34 @@ points(ssaplus1.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dashed")
 points(ssaplus2.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dotted")
 mtext(side = 3, c("(d)"), adj = 0)
 legend("bottomright",  c("SSAn", "SSA+ (inc. 1)", "SSA+ (inc. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
-
-########## Fluctuating
-# Parameters, logistic growth (fluctuating1)
+# Logistic growth (fluctuating)
 param1 <- list(b1 = .003,
               d1 = .0027,
               d2 = .000003,
               logisticfun,
               env = fluctuating1)
 param1 <- unlist(param1) # collapse to one-dimensional list
-# Parameters, logistic growth (fluctuating2)
 param2 <- list(b1 = .003,
               d1 = .0027,
               d2 = .000003,
               logisticfun,
               env = fluctuating2)
 param2 <- unlist(param2) # collapse to one-dimensional list
+## Create state change array
+pproc <- matrix(c(1,
+                  -1),
+                nrow = 2)
+## Times
+times <- seq(0, 5, .1)
 ## Make CDFs
 ssa.cdf <- c()
 ssaplus1.cdf <- c()
 ssaplus2.cdf <- c()
 for(i in 1:length(times)){
     ssa.cdf[i] <- 1 - exp(-sum(logisticINT(0, init, param1)) * times[i])
-    ssaplus1.cdf[i] <- nhppCDF(times[i], init, param1, logisticINT)
-    ssaplus2.cdf[i] <- nhppCDF(times[i], init, param2, logisticINT)
+    ssaplus1.cdf[i] <- nhpptimes(times[i], init, param1, logisticINT)
+    ssaplus2.cdf[i] <- nhpptimes(times[i], init, param2, logisticINT)
 }
-# Plot
 plot(NA, xlab = "Time", ylab = "",
      xlim = c(0, 5), ylim = c(0, 1),
      xaxt = "n", yaxt = "n", bty = "n",
@@ -224,32 +286,48 @@ points(ssaplus1.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dashed")
 points(ssaplus2.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dotted")
 mtext(side = 3, c("(e)"), adj = 0)
 legend("bottomright",  c("SSAn", "SSA+ (fluc. 1)", "SSA+ (fluc. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
-
-########## Random
-# Parameters, logistic growth (random1)
+# Exponential growth (irregular 1)
+## Create irregular environmental values
+set.seed(20170731)
+randenv1 <- rnorm(length(times), mean = 1.5, sd = .25)
+## View values
+randenv1
+# Exponential growth (irregular 2)
+## Create function that interpolates these values
+randenvfun1 <- splinefun(times, randenv1)
+set.seed(20170731)
+randenv2 <- rnorm(length(times), mean = 1.5, sd = .5)
+## View values
+randenv2
+## Create function that interpolates these values
+randenvfun2 <- splinefun(times, randenv2)
 param1 <- list(b1 = .003,
               d1 = .0027,
               d2 = .000003,
               logisticfun,
               env = randenvfun1)
 param1 <- unlist(param1) # collapse to one-dimensional list
-# Parameters, logistic growth (random2)
 param2 <- list(b1 = .003,
               d1 = .0027,
               d2 = .000003,
               logisticfun,
               env = randenvfun2)
 param2 <- unlist(param2) # collapse to one-dimensional list
+## Create state change array
+pproc <- matrix(c(1,
+                  -1),
+                nrow = 2)
+## Times
+times <- seq(0, 5, .1)
 ## Make CDFs
 ssa.cdf <- c()
 ssaplus1.cdf <- c()
 ssaplus2.cdf <- c()
 for(i in 1:length(times)){
     ssa.cdf[i] <- 1 - exp(-sum(logisticINT(0, init, param1)) * times[i])
-    ssaplus1.cdf[i] <- nhppCDF(times[i], init, param1, logisticINT)
-    ssaplus2.cdf[i] <- nhppCDF(times[i], init, param2, logisticINT)
+    ssaplus1.cdf[i] <- nhpptimes(times[i], init, param1, logisticINT)
+    ssaplus2.cdf[i] <- nhpptimes(times[i], init, param2, logisticINT)
 }
-# Plot
 plot(NA, xlab = "Time", ylab = "",
      xlim = c(0, 5), ylim = c(0, 1),
      xaxt = "n", yaxt = "n", bty = "n",
@@ -260,6 +338,5 @@ points(ssa.cdf ~ times, type = "l", col = "black", lwd = 2)
 points(ssaplus1.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dashed")
 points(ssaplus2.cdf ~ times, type = "l", col = "black", lwd = 2, lty = "dotted")
 mtext(side = 3, c("(f)"), adj = 0)
-legend("bottomright",  c("SSAn", "SSA+ (rand. 1)", "SSA+ (rand. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
-
-#dev.off()
+legend("bottomright",  c("SSAn", "SSA+ (irreg. 1)", "SSA+ (irreg. 2)"), bty = "n", inset = .02, adj = c(0, 0), lty = c("solid", "dashed", "dotted"), lwd = 2)
+dev.off()
